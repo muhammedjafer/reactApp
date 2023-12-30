e995df2f47a67c780e04200e49e7d1aaf38269c5
muhammedjafer <mohammadjafer990@gmail.com>
2023-12-30 22:53:27 +0300

    use state in react

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import 'bulma/css/bulma.css';
-import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CartanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import { useState } from 'react';
+
+function getRandomAnimal() {
+    const animals = [
+        'bird',
+        'cat',
+        'cow',
+        'dog',
+        'gator',
+        'horse'
+    ];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
 
 export default function App() {
-    return (
-        <div>
-            <section className='hero is-primary'>
-                <div className='hero-body'>
-                    <p className='title'>Personal Digital Assistants</p>
-                </div>
-            </section>
 
-            <div className='container'>
-                <section className='section'>
-                    <div className='columns'>
-                        <div className='column is-4'>
-                            <ProfileCard color="red" 
-                            image={AlexaImage} 
-                            description="Alexa created by amazon"
-                            />
-                        </div>
-                        <div className='column is-4'>
-                            <ProfileCard color="blue" 
-                            image={CartanaImage} 
-                            description="Cortana is useless"
-                            />
-                        </div>
-                        <div className='column is-4'>
-                            <ProfileCard color="yellow" 
-                            image={SiriImage} 
-                            description="Siri was made by Apple" />
-                        </div>
-                    </div>
-                </section>
-            </div>
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    return (
+        <div className='m-3'>
+            <button onClick={handleClick} className='button'>Add Animal: </button>
+        <div>{animals}</div>
         </div>
     );
 }
